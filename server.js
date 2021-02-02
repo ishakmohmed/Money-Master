0036afdbc181747547f179475d9303697087144a
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-02-03 00:35:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ if (process.env.NODE_ENV === "production") {
 
   app.get(
     "*",
-    (req, res) =>
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html")) // node path module: from current dir, go to client > build > index.html
+    (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); // node path module: from current dir, go to client > build > index.html
+    }
   );
 }
 
